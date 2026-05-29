0727fab879269b9e193163b0df96636196dccb9b
TylerBa1ley <tyb0025@arastudent.ac.nz>
2026-05-29 22:09:38 +1200

    Add files via upload

---
+++ Assessment 2 Iteration/icons.js
@@ -0,0 +1,14 @@
+const almondIcon = L.icon({
+    iconUrl: 'images/almond-icon.png', 
+    iconSize: [7],  
+    iconAnchor: [7, 8],          
+    popupAnchor: [0, -32]             
+});
+
+const treeIcons = {
+    "Oak": oakIcon
+    "Almond": almondIcon,
+    "English Oak": oakIcon,
+    "Pin Oak": oakIcon, 
+    "Maiden's Gum": L.icon({ iconUrl: 'images/gum.png', iconSize: [7] })
+};
